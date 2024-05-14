9954d4ab4d6b8522221333e1f2987dfc267bfe45
felipeminuzzi <feminuzzi@hotmail.com>
2024-05-14 15:36:15 -0300

    improve code

--- src/lstm_2D.py
+++ src/lstm_2D.py
@@ -2,13 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import xarray as xr
-import sys
-#import tensorflow as tf
-import csv
+import tensorflow as tf
 import time
-import warnings
 import multiprocessing as mp
-from sklearn import preprocessing
+
+import warnings
 warnings.filterwarnings('ignore')
 
 def get_data(dataset, lat, lon):
@@ -62,23 +60,30 @@ def erro(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.abs((y_true - y_pred)/y_true)*100
 
-def split_sequence(sequence, n_steps_in, lead_time, flag):
+def split_sequence(sequence, sequence2, n_steps_in, lead_time):
     X, y = [], []
     m = len(sequence) - lead_time
     for i in range(m):
         end_ix     = i + n_steps_in
-        out_end_ix = end_ix + int(n_steps_in*2)
-        if flag == True:
-            if out_end_ix > m:
-                break
-            seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
-        else:
-            if end_ix > m:
-                break
-            seq_x, seq_y = sequence[i:end_ix, 1:], sequence[end_ix+lead_time-1, 0]
+
+        if end_ix > m:
+            break
+        seq_x, seq_y = sequence[i:end_ix, 0:], sequence2[end_ix+lead_time-1]
         X.append(seq_x)
         y.append(seq_y)
-    return np.array(X), np.array(y) 
+    return np.array(X), np.array(y)
+
+def prepare_data(X, y, num_features):
+    dim_1                        = X.shape[0]
+    dim_2                        = X.shape[1]
+    dim_y                        = y.shape[0]
+
+    X                            = X.flatten()
+    y                            = y.flatten()
+    X                            = X.reshape((dim_1, dim_2, num_features))
+    y                            = y.reshape((dim_y, 1, 1))
+    
+    return X,y 
 
 def compile_and_fit(model, X, y, patience=20):
     early_stopping           = tf.keras.callbacks.EarlyStopping(
@@ -97,7 +102,7 @@ def compile_and_fit(model, X, y, patience=20):
         epochs               = 1,
         validation_split     = 0.2,
         callbacks            = [early_stopping],
-        verbose              = 0
+        verbose              = 1
     )
     return history
 
@@ -132,56 +137,66 @@ def plot_future(df,mape_value):
     plt.title(f'Previsão de altura de onda futuro. MAPE da validação: {mape_value}')
     plt.savefig(f'lstm_predicts_future.png', bbox_inches='tight')
 
-def get_model(num_prev, num_features):
+def get_model(num_prev, forecast, num_features):
     modelo             = tf.keras.Sequential([
-        tf.keras.layers.LSTM(64, activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
-        tf.keras.layers.LSTM(48, activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
-        tf.keras.layers.LSTM(32, activation='relu', return_sequences=False),
+        tf.keras.layers.LSTM(256,  activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
+        tf.keras.layers.LSTM(128, activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
+        tf.keras.layers.LSTM(64,  activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
+        tf.keras.layers.LSTM(48,  activation='relu', return_sequences=True, input_shape=(forecast, num_features)),
+        tf.keras.layers.LSTM(32,  activation='relu', return_sequences=False),
         tf.keras.layers.Dense(num_prev, kernel_initializer=tf.initializers.zeros)])
     return modelo
 
-def create_future_predict(lead, df, npredict, forecast, num_features):
-    if (lead==6):
+def create_train_test(df, npredict, lead, tgt = 'Hs'):
+    if lead == 0:
         count = 0
-    elif (lead==12):
+    elif lead==6:
+        count = 0
+    elif lead==12:
         count = 6
-    elif (lead==18):
+    elif lead==18:
         count = 12
     else:
         count = 18
+    
+    target                   = df[tgt][:-(npredict+count)]
+    target_predict           = df[tgt][-(npredict+count):]
 
-    inputs_1                 = df['Hs'][:-(npredict+count)].values
-    inputs_2                 = df['Period'][:-(npredict+count)].values
-    inputs_3                 = df['10m-direc'][:-(npredict+count)].values
-    inputs_4                 = df['10m-speed'][:-(npredict+count)].values
+    df.drop([tgt], axis=1, inplace=True)
+    datas                    = []
 
-    inputs_1                 = inputs_1.reshape((len(inputs_1), 1))
-    inputs_2                 = inputs_2.reshape((len(inputs_2), 1))
-    inputs_3                 = inputs_3.reshape((len(inputs_3), 1))
-    inputs_4                 = inputs_4.reshape((len(inputs_4), 1))
+    for col in df.columns:
+        inputs_1             = df[col][:-(npredict+count)].values
+        inputs_1             = inputs_1.reshape((len(inputs_1), 1))
+        datas.append(inputs_1)
+    
+    inputs                   = np.hstack(datas)
 
-    inputs                   = np.hstack((inputs_1, inputs_2, inputs_3, inputs_4))
-    X, y                     = split_sequence(inputs, forecast, lead, False)
+    predict_data = []
+    for col in df.columns:
+        inputs_2             = df[col][-(npredict+count):].values
+        inputs_2             = inputs_2.reshape((len(inputs_2), 1))
+        predict_data.append(inputs_2)
 
-    model                    = get_model(1, num_features)
+    x_input                  = np.hstack(predict_data)
+    
+    return inputs, target, x_input, target_predict
+
+def future_predict(lead, df, npredict, forecast, num_features):
+
+    train_input, train_target, test_input, test_target    = create_train_test(df, npredict, lead)
+    x_train, y_train                                      = split_sequence(train_input, train_target, forecast, lead)
+    X, y                                                  = prepare_data(x_train, y_train, num_features)
+
+    x_test, y_test                                        = split_sequence(test_input, test_target, forecast, lead)
+    x_in, labels                                          = prepare_data(x_test, y_test, num_features)
+    
+    model                    = get_model(1, forecast, num_features)
     history                  = compile_and_fit(model, X, y)
-
-    x_input_1                = df['Hs'][-(npredict+count):].values
-    x_input_2                = df['Period'][-(npredict+count):].values
-    x_input_3                = df['10m-direc'][-(npredict+count):].values
-    x_input_4                = df['10m-speed'][-(npredict+count):].values
-
-    x_input_1                = x_input_1.reshape((len(x_input_1), 1))
-    x_input_2                = x_input_2.reshape((len(x_input_2), 1))
-    x_input_3                = x_input_3.reshape((len(x_input_3), 1))
-    x_input_4                = x_input_4.reshape((len(x_input_4), 1))
-
-    x_input                  = np.hstack((x_input_1, x_input_2, x_input_3, x_input_4))
-    x_in, labels             = split_sequence(x_input, forecast, lead, False)
     
     result                   = pd.DataFrame()
-    result['Data']           = df.index[-npredict+forecast+5:]
-    result['Hs Reanalysis Value'] = labels
+    result['Data']           = test_target.index[-len(labels):]
+    result['Hs Reanalysis Value'] = y_test
     
     predictions              = []
     predictions              = [prediction(model, dado, forecast, num_features) for dado in x_in]
@@ -210,21 +225,19 @@ df_latlong   = pd.DataFrame(dict(long=xv.ravel(), lat=yv.ravel()))
 lst_latlong  = df_latlong.values
 
 for x in lst_latlong:
-    df_train = create_train_dataset(x,data_era, add_step)
-    breakpoint()
+    df_train     = create_train_dataset(x, data_era, add_step)
 
-column_ind   = {name: i for i, name in enumerate(rg_rea.columns)}
+    lead_time    = [0,6,12,18,24]
+    forecast     = 12
+    num_features = df_train.shape[1] - 1
+    npredict     = 744
 
-lead_time    = [6,12,18,24]
-forecast     = 12
-num_features = 3
-npredict     = 744
-
-start                        = time.time()
-for lead in lead_time:
-    create_future_predict(lead, rg_rea, npredict, forecast, num_features)
-
-end                          = time.time()
+    start                        = time.time()
+    for lead in lead_time:
+        future_predict(lead, df_train, npredict, forecast, num_features)
+        breakpoint()
+        
+    end                          = time.time()
 
 print('Time of execution: ',(end-start)/60, ' minutes.')
 print('###############################################')
